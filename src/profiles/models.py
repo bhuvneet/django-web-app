5a7a6f31b46e60813c54ba571f27f8704055fd6d
bhuvneet <bhuvneet3@gmail.com>
2023-03-11 17:39:19 -0500

    Created models for the project.

---
+++ src/profiles/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)  # optional field
+    avatar = models.ImageField(default='avatar.png', upload_to='avatars')   # folder where user images will be saved
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"profile of the user {self.user.username}"
