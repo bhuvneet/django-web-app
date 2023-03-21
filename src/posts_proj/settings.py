c5836f1c5554809f0bdbb93a19c0cd1a3e000294
bhuvneet <bhuvneet3@gmail.com>
2023-03-21 17:05:04 -0400

    Completed first AJAX call.

--- src/posts_proj/settings.py
+++ src/posts_proj/settings.py
@@ -122,9 +122,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "static", 
+    BASE_DIR / 'static', 
     BASE_DIR / 'posts' / 'static',
     BASE_DIR / 'profiles' / 'static',
 ]
