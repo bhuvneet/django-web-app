0f2e98779321f8f601103e9bfbcb88d864b154ab
bhuvneet <bhuvneet3@gmail.com>
2023-04-06 12:46:11 -0400

    Added photos to post using Carousel.

--- src/posts/static/posts/main.js
+++ src/posts/static/posts/main.js
@@ -34,6 +34,12 @@ const getCookie = (name) => {
 const csrftoken = getCookie('csrftoken');
 console.log("heelo:", csrftoken)
 
+const deleted = localStorage.getItem('title')
+if(deleted){
+    handleAlerts('danger', `deleted "${deleted}"`)
+    localStorage.clear()
+}
+
 const likeUnlikePosts = () => {
     const likeUnlikeForms = [...document.getElementsByClassName('like-unlike-forms')]
     console.log(likeUnlikeForms)
